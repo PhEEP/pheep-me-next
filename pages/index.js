12fc798bdf1823d36bc338d55874c1ef1f81fd88
PhEEP <philippealmoore@gmail.com>
2018-10-27 20:46:28 -0700

    Typography start

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import "../utils/typography";
+
+export default () => (
+  <section>
+    <h1>Pheep Me Next</h1>
+    <h2>The next steps for Pheep Me</h2>
+    <p>
+      Goal is to use nextjs, typographyjs, contentful, graphql, appollo (I
+      think)
+    </p>
+    <ul>
+      <li>Typography.js</li>
+      <li>Setup a layout</li>
+      <li>CMS</li>
+      <li>...</li>
+      <li>Profit!</li>
+    </ul>
+  </section>
+);
